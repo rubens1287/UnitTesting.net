aab4e5f46311b1be99ec38583b3638ecb5d7b8cb
de Medeiros Lobo <rslo@gft.com>
2022-07-26 09:24:34 -0300

    Implementado testes para calculadora

--- UnitTesting.Test/CalculadoraTest.cs
+++ UnitTesting.Test/CalculadoraTest.cs
@@ -6,7 +6,7 @@ namespace UnitTesting.Tests
     public class CalculadoraTest
     {
         [Fact]
-        public void TestSoma()
+        public void ValidaSoma()
         {
             //Arange
             int valor1 = 10;
@@ -22,7 +22,39 @@ namespace UnitTesting.Tests
         }
 
         [Fact]
-        public void TestDivisaoComZeroDivisor()
+        public void ValidaMultiplicacao()
+        {
+            //Arange
+            int valor1 = 2;
+            int valor2 = 2;
+            int expect = 4;
+            int result;
+
+            //Act
+            result = Calculadora.Multiplicao(valor1, valor2);
+
+            //Assert
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void ValidaSubtracao()
+        {
+            //Arange
+            int valor1 = 10;
+            int valor2 = 2;
+            int expect = 8;
+            int result;
+
+            //Act
+            result = Calculadora.Subtracao(valor1, valor2);
+
+            //Assert
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void ValidaDivisaoComZeroDivisor()
         {
             //Arange
             int dividento = 10;
@@ -36,5 +68,23 @@ namespace UnitTesting.Tests
             //Assert
             Assert.Equal(expect, result);
         }
+
+        [Fact]
+        public void ValidaDivisaoComDivisorMaiorQueZero()
+        {
+            //Arange
+            int dividento = 10;
+            int divisor = 2;
+            int expect = 5;
+            int result;
+
+            //Act
+            result = Calculadora.Divisao(dividento, divisor);
+
+            //Assert
+            Assert.Equal(expect, result);
+        }
+
+
     }
 }
