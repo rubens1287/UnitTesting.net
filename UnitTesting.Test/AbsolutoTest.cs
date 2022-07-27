daaf467b11aa6bdc8fb12f62b50b745bafdea4b0
de Medeiros Lobo <rslo@gft.com>
2022-07-27 16:50:57 -0300

    Pronto para treinamento

--- UnitTesting.Test/AbsolutoTest.cs
+++ UnitTesting.Test/AbsolutoTest.cs
@@ -5,36 +5,6 @@ namespace UnitTesting.Test
     
     public class AbsolutoTest
     {
-        [Fact]
-        public void TestComValorMenorQueZero()
-        {
-            //Arrange
-            Absoluto absoluto = new Absoluto();
-            int actual;
-            int expect = 1;
-
-            //Act
-            actual = absoluto.retornaValorAbsoluto(-1);
-
-            //Assert
-            Assert.Equal(expect, actual);
-
-        }
-
-        [Fact]
-        public void TestComValorZero()
-        {
-            //Arrange
-            Absoluto absoluto = new Absoluto();
-            int actual;
-            int expect = 0;
-
-            //Act
-            actual = absoluto.retornaValorAbsoluto(0);
-
-            //Assert
-            Assert.Equal(expect, actual);
-
-        }
+        
     }
 }
--- UnitTesting.Test/FatorialTest.cs
+++ UnitTesting.Test/FatorialTest.cs
@@ -4,53 +4,6 @@ namespace UnitTesting.Test
 {
     public class FatorialTest
     {
-        [Fact]
-        public void TestComValorMenorQueZero()
-        {
-            //Arrange
-            Fatorial fatorial = new Fatorial();
-            int actual;
-            int expect = 0;
-
-            //Act
-            actual = fatorial.resultadoFatorial(-1);
-
-            //Assert
-            Assert.Equal(expect, actual);
-
-        }
-
-        [Fact]
-        public void TestComValorIgualZero()
-        {
-            //Arrange
-            Fatorial fatorial = new Fatorial();
-            int actual;
-            int expect = 1;
-
-            //Act
-            actual = fatorial.resultadoFatorial(0);
-
-            //Assert
-            Assert.Equal(expect, actual);
-
-        }
-
-
-        [Fact]
-        public void TestComValorMaiorQueZero()
-        {
-            //Arrange
-            Fatorial fatorial = new Fatorial();
-            int actual;
-            int expect = 6;
-
-            //Act
-            actual = fatorial.resultadoFatorial(3);
-
-            //Assert
-            Assert.Equal(expect, actual);
-
-        }
+       
     }
 }
